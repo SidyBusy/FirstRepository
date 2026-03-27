596ec28d977cd5138c6e1995fc8fdd0b3d008fbc
SidyBusy <saidgaynutdinov@gmail.com>
2026-03-27 19:04:45 +0300

    new commit

--- main.cpp
+++ main.cpp
@@ -5,51 +5,21 @@
 #include <stack>
 #include <queue>
 #include <set>
+#include <random>
+#include <chrono>
+#include <thread>
+
 
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++) {
-        vector<long long> arr;
-        int n;
-        cin >> n;
-        for (int j = 0; j < n; j++) {
-            long long num;
-            cin >> num;
-            arr.push_back(num);
-        }
-
-        int ans = 0;
-        long long mn = arr[0];
-        long long mx = arr[0];
-        int cnt = 1;
-        for (int j = 1; j < n; j++) {
-            if (arr[j] > mn && arr[j] < mx) {
-                cnt++;
-            }
-            else if (arr[j] == mx && arr[j - 1] == mx) {
-                cnt++;
-            }
-            else if (arr[j] == mx + 1 && arr[j - 1] == mx) {
-                mx = arr[j];
-                cnt++;
-            }
-            else {
-                ans++;
-                mn = arr[j];
-                mx = arr[j];
-                cnt = 1;
-            }
-        }
-
-        ans += 1;
-        cout << ans << '\n';
-
-    }
-
     
+    string a = "this is the simple example of how delayed output works.";
+    this_thread::sleep_for(chrono::seconds(2));
+    for (auto el : a) {
+        cout << el << flush;
+        if (el != ' ') {
+            this_thread::sleep_for(chrono::milliseconds(100));
+        }
+    }
 }
